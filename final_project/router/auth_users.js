3cf01d67c87e7cb9517a939f9d4a4e16f5e02df5
unknown <atishsuslade@gmail.com>
2023-10-02 03:46:39 +0530

    updated code

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -3,11 +3,7 @@ const jwt = require('jsonwebtoken');
 let books = require("./booksdb.js");
 const regd_users = express.Router();
 
-let users = [
-    {'username':'atish','password':'suslade'},
-    {'username':'manish','password':'suslade'},
-    {'username':'samuel','password':'samuel@123'},
-];
+let users = [];
 
 const isValid = (username) => { //returns boolean
     //write code to check is the username is valid
